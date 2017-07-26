c0222e6a62035de132886575a61a2f3e96a069d0
aurelienperillat <perillat_botto@et.esiea.fr>
2017-07-26 15:25:58 +0200

    changeOwner

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -54,6 +54,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "changeOwner" {
+		return t.write(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
@@ -86,6 +88,24 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	key = args[0] //rename for funsies
 	value = args[1]
 	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SimpleChaincode) changeOwner(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var asset, newOwner string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	asset = args[0] //rename for funsies
+	newOwner = args[1]
+	err = stub.PutState(asset, []byte(newOwner)) //write the variable into the chaincode state
 	if err != nil {
 		return nil, err
 	}
